34c96edf089706f894f971f258e9efe1d70475d3
Martin <240703@vutbr.cz>
2023-03-25 19:54:51 +0100

    Added class Drug, edited main

---
+++ Drug database/Drug.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drug_database
+{
+    internal class Drug
+    {
+        public long ID = 0;
+        public string Name = null;
+        public string Description = null;
+        public string Producer = null;
+        private int InStock = 0;
+        private double Price = 0;
+        private string PhotoPath = null;
+
+        public Drug(long ID, string name, string desctription, string producer, int inStock, double price, string photoPath)
+        {
+            this.ID = ID;
+            this.Name = name;
+            this.Description = desctription;
+            this.Producer = producer;
+            this.InStock = inStock;
+            this.Price = price;
+            this.PhotoPath = photoPath;
+        }
+
+        
+    }
+}
